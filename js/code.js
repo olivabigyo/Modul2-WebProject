c4d949d6e0385bb4638dfcd49ae4e7e661b35458
Kata <35190838+olivabigyo@users.noreply.github.com>
2020-04-15 13:49:04 +0200

    menu color: scrol event

--- js/code.js
+++ js/code.js
@@ -2,16 +2,20 @@
 
 window.addEventListener('scroll', myfunction);
 
+const menu = document.querySelector('nav #menu-tag li a');
+console.log(menu);
 const mypicture = document.querySelector('.hero-img');
-const fade_start = 200;
+const fade_start = 0;
 const fade_end = 400;
 let last_opacity = 1;
 
 function myfunction() {
+    menu.style.color = 'black';
     const scrollHeight = window.scrollY;
     const opacity = Math.min(1, Math.max(0, (fade_end - scrollHeight) / (fade_end - fade_start)));
     if (opacity == last_opacity) return;
     console.log(scrollHeight, opacity);
     mypicture.style.opacity = opacity;
     last_opacity = opacity;
+    if (opacity == 0) {menu.style.color = 'black'} else {menu.style.color = 'white'};
 }
